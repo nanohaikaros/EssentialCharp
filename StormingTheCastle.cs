defd1e98f2586663ce1baec03ad5255eea950b46
Nanoha <1371689491@qq.com>
2021-08-19 10:25:54 +0800

    1

---
+++ StormingTheCastle.cs
@@ -0,0 +1,21 @@
+class StormingTheCastle
+{
+    static void Main()
+    // {
+    //     string valerie;
+    //     string miracleMax = "Have fun storming the castle!";
+
+    //     valerie = "Think it will work?";
+
+    //     System.Console.WriteLine(miracleMax);
+    //     System.Console.WriteLine(valerie);
+
+    //     miracleMax = "It would take a miracle.";
+    //     System.Console.WriteLine(miracleMax);
+    // }
+
+    {
+        string requirements, miracleMax;
+        requirements = miracleMax = "It world take a miracle.";
+    }
+}
---
+++ miracleMax.cs
@@ -0,0 +1,11 @@
+class miracleMax
+{
+    static void Main()
+    {
+        string max;
+
+        max = "Have fun storming the castle!";
+
+        System.Console.WriteLine(max);
+    }
+}
